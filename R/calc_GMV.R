a3cc239871e038c4ae860b77b2471fcc52252c1c
DanRouillard <79175775+DanRouillard@users.noreply.github.com>
2021-04-28 21:25:46 -0400

    Update calc_GMV.R
    to upper dropped

--- R/calc_GMV.R
+++ R/calc_GMV.R
@@ -15,7 +15,7 @@ calc_GMV <- function(GTV, QMD,  SppId, Origin = "N", PlantedSpp = NA) {
   if (!(SppId %in% MISTR_coef$species_model_selection$SppId)) {stop(paste0("Wrong species code: ",SppId))}
 
   #get species code based on species_model_selection table
-  SppId <- toupper(SppId)
+  #SppId <- toupper(SppId)
   current_coefs <- MISTR_coef$species_model_selection[MISTR_coef$species_model_selection$SppId == SppId,]
   SppID_GMV <- current_coefs$YcId
 
