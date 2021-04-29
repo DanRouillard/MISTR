4cdb2fc577de1c6669eccceb28de3d4f975b141f
DanRouillard <79175775+DanRouillard@users.noreply.github.com>
2021-04-28 21:22:45 -0400

    Update calc_BA.R
    to upper dropped

--- R/calc_BA.R
+++ R/calc_BA.R
@@ -18,7 +18,7 @@ calc_BA <- function(SI, BHage, SppId,  stocking=1, Origin = "N", PlantedSpp = NA
   if (!(SppId %in% MISTR_coef$species_model_selection$SppId)) {stop(paste0("Wrong species code: ",SppId))}
 
   #get species code based on species_model_selection table
-  SppId <- toupper(SppId)
+  #SppId <- toupper(SppId)
   current_coefs <- MISTR_coef$species_model_selection[MISTR_coef$species_model_selection$SppId == SppId,]
   SppID_BA <- current_coefs$YcBaId
 
